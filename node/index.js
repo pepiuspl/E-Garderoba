7832af918a1142ab5fa03d8b0ed96f99865cc218
Tom <tom@E-Garderoba.local>
2025-09-11 17:50:14 +0000

    cool

--- node/index.js
+++ node/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config({ path: "/home/tom/E-Garderoba/.env" });
 console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 console.log("Loaded env:", process.env);
