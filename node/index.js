f2bc5c8752749af88b2e1f900ea96508b87f6445
pepiuspl <pepiuspl@gmail.com>
2025-09-13 15:01:27 +0200

    mutler path fix

--- node/index.js
+++ node/index.js
@@ -26,7 +26,7 @@ const path = require("path");
 // folder na zdjęcia
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, path.join(__dirname, "../uploads"));
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
